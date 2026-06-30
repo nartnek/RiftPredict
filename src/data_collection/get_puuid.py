aaadbc223447c95cf5cf4d4de767c99fe814be86
Xuan Ming (Mark) Cao <xmc2@sfu.ca>
2026-06-29 19:00:57 -0700

    Automate Riot data collection pipeline

--- src/data_collection/get_puuid.py
+++ src/data_collection/get_puuid.py
@@ -3,7 +3,6 @@ import argparse
 import requests
 from urllib.parse import quote
 
-## to run this file: python3 get_puuid.py --game-name MunchyPunchyLOL --tag-line TTV1
 
 API_KEY = os.getenv("RIOT_API_KEY")
 
@@ -12,32 +11,41 @@ if not API_KEY:
 
 REGION = "americas"
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--game-name", required=True, help="Riot ID game name")
-    parser.add_argument("--tag-line", required=True, help="Riot ID tag line")
-    args = parser.parse_args()
 
-    game_name_encoded = quote(args.game_name)
-    tag_line_encoded = quote(args.tag_line)
+def get_puuid(game_name, tag_line):
+    game_name_encoded = quote(game_name)
+    tag_line_encoded = quote(tag_line)
 
     url = (
         f"https://{REGION}.api.riotgames.com/riot/account/v1/accounts/"
         f"by-riot-id/{game_name_encoded}/{tag_line_encoded}"
     )
 
-    headers = {"X-Riot-Token": API_KEY}
+    headers = {
+        "X-Riot-Token": API_KEY
+    }
 
     response = requests.get(url, headers=headers)
 
-    print("Status code:", response.status_code)
-
-    if response.status_code == 200:
-        data = response.json()
-        print("\nPUUID:")
-        print(data["puuid"])
-    else:
+    if response.status_code != 200:
+        print("Error:", response.status_code)
         print(response.text)
+        response.raise_for_status()
+
+    data = response.json()
+    return data["puuid"]
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--game-name", required=True)
+    parser.add_argument("--tag-line", required=True)
+    args = parser.parse_args()
+
+    puuid = get_puuid(args.game_name, args.tag_line)
+    print("PUUID:")
+    print(puuid)
+
 
 if __name__ == "__main__":
     main()
---
+++ test_feature_engineering.py
@@ -0,0 +1,35 @@
+# This is for testing the feature engineering functions. It is not part of the main application.
+from src.feature_engineering.feature_builder import build_features
+from src.feature_engineering.load_champion_data import load_champion_data
+from  src.feature_engineering.composition_features import get_team_composition_features
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).parent
+
+champion_data = load_champion_data(PROJECT_ROOT / "data" / "champions.json")
+
+blue_team = [
+    "Aatrox",
+    "Ahri",
+    "Ashe",
+    "Leona",
+    "LeeSin"
+]
+
+red_team = [
+    "Darius",
+    "Diana",
+    "Draven",
+    "Jinx",
+    "Thresh"
+]
+
+features = get_team_composition_features(blue_team, champion_data)
+
+team_features = build_features(blue_team, red_team, champion_data)
+# for team blue
+print(features)
+
+# for both teams
+print(team_features)
+
